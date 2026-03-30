612827f7df8b6399c0e431bbecc7a8bb77192c8d
Harshit Gupta <153393197+Ashusf90@users.noreply.github.com>
2026-03-30 12:38:13 +0530

    Create schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Users Table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
